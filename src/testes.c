c0077c75b1404ab92968dd1f7781921ebd12f1f6
endelgs <endel.gs@gmail.com>
2013-01-26 16:20:42 -0800

    Primeiro commit da segunda fase

--- src/testes.c
+++ src/testes.c
@@ -1,75 +1,98 @@
+// CABECALHOS DEFAULT
+
 #include <malloc.h>
 #include <stdlib.h>
 #include <stdio.h>
 typedef int boolean;
 #define true 1
 #define false 0
-typedef
-void (*Func)();
-// class A { ... }
+typedef void (*Func)();
 
-typedef
-struct _St_A {
-  Func *vt;
-  // variavel de instancia i da classe A
-  int _A_i;
+// DEFINICAO DA ESTRUTURA DA CLASSE A
+
+typedef struct _St_A {
+  Func * vt;
+  int _A_a;
 } _class_A;
-_class_A *new_A(void);
 
-int _A_get(_class_A *this) {
-  return this->_A_i;
-}
-
-void _A_put(_class_A *this, int _p_i) {
-  this->_A_i = _p_i;
-}
-// tabela de metodos da classe A -- virtual table
+// Vetor de metodos publicos da classe A
 Func VTclass_A[] = {
-  (void (*)()) _A_get,
-  (void (*)()) _A_put
-};
+  _A_m1,
+  _A_m2,
+  _A_m3,
+  _A_m4
+}; // DEFINICAO DA ESTRUTURA DA CLASSE Program
 
-class_A *new_A() {
-  _class_A *t;
-  if ((t = malloc(sizeof (_class_A))) != NULL)
-    t->vt = VTclass_A;
-  return t;
-}
-
-typedef
-struct _St_Program {
-  Func *vt;
+typedef struct _St_Program {
+  Func * vt;
 } _class_Program;
-_class_Program *new_Program(void);
 
-void _Program_run(_class_Program *this) {
-  // A a;
-  _class_A *_a;
-  // int k;
-  int _k;
-  // a = new A();
-  _a = new_A();
-  // a.put(5);
-  ((void (*)(_class_A *, int)) _a->vt[1])(_a, 5);
-  // k = a.get();
-  k = ((int (*)(_class_A *)) _a->vt[0])(_a);
-  // write(k);
-  printf("%d ", _k);
-}
+// Vetor de metodos publicos da classe Program
 Func VTclass_Program[] = {
-  (void (*)()) _Program_run
-};
+  _Program_run
+}; // PROTOTIPOS dos metodos da classe A
 
-_class_Program *new_Program() {
-  _class_Program *t;
-  if ((t = malloc(sizeof (_class_Program))) != NULL)
-    t->vt = VTclass_Program;
-  return t;
+void _A_m1(_class_A *this, int _n);
+void _A_m2(_class_A *this, int _n);
+void _A_m3(_class_A *this, int _n);
+void _A_m4(_class_A *this, char * _s);
+// PROTOTIPOS dos metodos da classe Program
+
+void _Program_run(_class_Program *this);
+// Imprimindo a lista de METODOS PUBLICOS da classe A
+
+void * _A_m1(_class_A *this, int _n) {
+  {
+    char __s[512];
+    gets(__s);
+    sscanf(__s, "%d", &_A_a);
+  }
+  printf("%d ", 1);
+  printf("%d ", _n);
 }
+
+void * _A_m2(_class_A *this, int _n) {
+  printf("%d ", 2);
+  printf("%d ", _n);
+}
+
+void * _A_m3(_class_A *this, int _n) {
+  printf("%d ", 3);
+  printf("%d ", _n);
+}
+
+void * _A_m4(_class_A *this, char * _s) {
+}
+
+// Imprimindo a lista de METODOS PRIVADOS da classe A
+
+// Imprimindo a lista de METODOS PUBLICOS da classe Program
+
+void * _Program_run(_class_Program *this) {
+  _class_A _a;
+  char * _s;
+  {
+    char __s[512];
+    gets(__s);
+    _s = malloc(strlen(__s) + 1);
+    strcpy(_s, __s);
+  }
+  puts("");
+  puts("Ok-ger08");
+  puts("The output should be :");
+  puts("1 1 2 2 3 3");
+  _a = new_A();
+  _A_m1(_a, 1);
+  _A_m2(_a, 2);
+  _A_m3(_a, 3);
+}
+
+// Imprimindo a lista de METODOS PRIVADOS da classe Program
+
+// Codigo para o MAIN
 
 int main() {
   _class_Program *program;
-  /* crie objeto da classe Program e envie a mensagem run para ele */
   program = new_Program();
   ((void (*)(_class_Program *)) program->vt[0])(program);
   return 0;
